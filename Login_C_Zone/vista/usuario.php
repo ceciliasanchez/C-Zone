3002b53b592142735251dcb357542654c40e2695
Haagila <67285649+Haagila@users.noreply.github.com>
2020-07-19 17:12:00 -0500

    feedbacks

--- Login_C_Zone/vista/usuario.php
+++ Login_C_Zone/vista/usuario.php
@@ -47,62 +47,7 @@ if (isset($_SESSION["usuario"])) {
       google.charts.load('current', {'packages':['corechart']});
       google.charts.setOnLoadCallback(drawChart);
 
-      function drawChart() {
-
-        var data = google.visualization.arrayToDataTable([
-          ['Task', 'Numero de infectados'],
-          ['San Sebastian',     64],
-          ['El Valle',   225],
-          ['El Sagrario',  5],
-          ['Punzara', 122],
-          ['Sucre',    340],
-          ['Carigan',    70]
-        ]);
-
-        var options = {
-          title: 'Numero De Infectados'
-        };
-
-        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
-
-        chart.draw(data, options);
-      }
-    </script>
-    <h1>COVID 19 en la Ciudad.</h1>
-  </head>
-  <body>
- 
-    <div id="piechart" style="width: 900px; height: 500px;"></div>
-  </body>
-
-
-        </p>
-        
-      </div>
-      <h4>Los coronavirus son una amplia familia de virus, algunos tienen la capacidad de
-   transmitirse de los animales a las personas. Producen cuadros clínicos que van desde 
-   el resfriado común hasta enfermedades más graves, como ocurre con el coronavirus que 
-   causó el síndrome respiratorio agudo grave (SARS-CoV) y el coronavirus causante del 
-   síndrome respiratorio de Oriente Medio (MERS-CoV).</h4>
-   <IMG SRC="https://www.paho.org/sites/default/files/styles/flexslider_full/public/hero/2020-03/covid-19-1190x574-2-full.jpg?h=fdc0eb87&itok=CerQCpzv" ALIGN=LEFT WIDTH=400 HEIGHT=200>
-   <br>
-	<br>
-	<br>
-  <br>
-		<br>
-		<br>
-    <br>
-		<br>
-		<br>
-    <br>
-		<br>
-		<br>
-   <h4>El nuevo coronavirus se llama SARS-CoV2, la enfermedad se llama Corona Virus Disease 2019=COVID19
-   Es un nuevo tipo de coronavirus que puede afectar a las personas y se ha detectado por 
-primera vez en diciembre de 2019 en la ciudad de Wuhan, provincia de Hubei, en China. 
-Todavía hay muchas cuestiones que se desconocen en relación a la enfermedad que produce: COVID-19.</h4>
-		</div>
-	</div>
+      
   <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
     <div class="modal-dialog">
       <div class="modal-content">
